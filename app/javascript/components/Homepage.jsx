40949895406c026f81d33fc1b9a62b8fb3fae760
ritwick malhotra <malhotraritwick2011@gmail.com>
2024-10-12 16:06:14 +0200

    fixes

--- app/javascript/components/Homepage.jsx
+++ app/javascript/components/Homepage.jsx
@@ -62,10 +62,17 @@ function Homepage() {
   }, []);
 
   useEffect(() => {
-    const handleResize = () => { setIsMobile(window.innerWidth < 768); };
+    const handleResize = () => { 
+      console.log(window.innerWidth);
+      setIsMobile(window.innerWidth < 768);
+    };
+
     window.addEventListener('resize', handleResize);
+
+    handleResize();
     return () => window.removeEventListener('resize', handleResize);
   }, []);
+
 
   useEffect(() => {
     if (selectedCity) {
